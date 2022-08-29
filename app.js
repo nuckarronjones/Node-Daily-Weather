868d2a0c3c6778337dcb1c5920676ff694021ccb
nuckarronjones <Scubaa4living@gmail.com>
2022-08-28 22:54:09 -0700

    A bunch of updates
    Added:
    -API data rendered on results page
    -Overlay and glowing elements to create an old terminal look
    -Tested post form submission to get city data
    -Design changes

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
-const app = require("express")();
+//VARIABLES AND REQUIREMENTS
+const express = require("express");
+const app = express();
 const request = require("request");
 
 const APIKEY = "0878968b63d46921556ce3918394c568";
-const location = "Pensacola";
-const URI = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${APIKEY}`;
+let location;
 
-const test = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}`
+//REGISTER VIEW ENGINE
+app.set("view engine", "ejs");
+app.set("views", "views"); //if you have your views in another folder
 
+//MIDDLEWARE
+app.use(express.static("public"));
+app.use(express.urlencoded());
+
+//ROUTES
 app.get("/", (req, res) => {
-
-  request({ url: test, json: true }, (err, response, body) => {
-    console.log(body)
-    if(!err){
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(body))
-    }else{
-        res.end("There was an error")
-    }
-  });
-
+  res.render("index.ejs");
 });
 
+app.get("/weather", (req, res) => {
+  //request module for calling API
+  request({ url: URI, json: true }, (err, response, body) => {
+    if (!err) {
+      res.render("results.ejs");
+    } else {
+      res.end("There was an error");
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  console.log("posted");
+
+  console.log(req.body);
+
+  location = req.body.search;
+
+  const URI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}`;
+
+  request({ url: URI, json: true }, (err, response, body) => {
+    if (!err) {
+      console.log(body);
+
+      console.log(URI);
+
+      res.render("results.ejs", { city: req.body.search.toUpperCase(), weatherData: body });
+    } else {
+      res.end("There was an error");
+    }
+  });
+});
+
+
+//404 error if no other pages are found first
+app.get("/404", (req, res) => {
+  res.render("404.ejs");
+});
+
+//start local server
 app.listen(3000, () => {
-  console.log("this is third");
   console.log("App is listening on port 3000");
 });
