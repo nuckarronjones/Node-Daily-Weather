6accc24b8ec8a2c8ddbe3ead9ba0fdda5f083d45
Nicholas Jones <Scubaa4living@gmail.com>
2023-01-06 19:49:41 -0600

    Tweaks for Render.com Error
    Error: Failed to lookup view "results.ejs" in views directory "/opt/render/project/src/views"

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ let location;
 
 //REGISTER VIEW ENGINE
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, './views')); //if you have your views in another folder
+app.set('views', path.join(__dirname, '/views')); //if you have your views in another folder
 
 //MIDDLEWARE
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.urlencoded());
 
 //ROUTES
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.get("/weather", (req, res) => {
   //request module for calling API
   request({ url: URI, json: true }, (err, response, body) => {
     if (!err) {
-      res.render("results.ejs");
+      res.render("results");
     } else {
       res.end("There was an error");
     }
@@ -41,10 +41,10 @@ app.post("/", (req, res) => {
     if (!err) {
       if (body.cod == "404") {
         //this is error handling if we DO get a response (unknown city for example), and theres not data we can use
-        res.render("404.ejs");
+        res.render("404");
       }
 
-      res.render("results.ejs", {
+      res.render("results", {
         city: req.body.search.toUpperCase(),
         weatherData: body,
       });
@@ -56,7 +56,7 @@ app.post("/", (req, res) => {
 
 //404 error if no other pages are found first
 app.get("/404", (req, res) => {
-  res.render("404.ejs");
+  res.render("404");
 });
 
 //start local server
