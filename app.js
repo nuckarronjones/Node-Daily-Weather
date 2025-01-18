bcebe3c2d51eea05a4de0fd0f93b3ed1decc337d
Nicholas Jones <njonesgrad@gmail.com>
2025-01-18 12:33:22 +0100

    Comments Cleanup / Logging
    Remove not needed comments - alongside any unnecessary console logs from old project.

--- app.js
+++ app.js
@@ -1,71 +1,48 @@
-//VARIABLES AND REQUIREMENTS
 const express = require("express");
 const app = express();
 const request = require("request");
-let path = require('path');
-//env port for hosting service
-let port = process.env.PORT || 3000
+let path = require("path");
+let port = process.env.PORT || 3000;
 
 const APIKEY = "c20388b143431a9ec198a11d39355e04";
 let location;
 
-//REGISTER VIEW ENGINE
 app.set("view engine", "ejs");
-app.set('views', __dirname + '/views'); //if you have your views in another folder
+app.set("views", __dirname + "/views");
 
-//MIDDLEWARE
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded());
 
-//ROUTES
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-// app.get("/weather", (req, res) => {
-//   //request module for calling API
-//   request({ url: URI, json: true }, (err, response, body) => {
-//     if (!err) {
-//       res.render("results");
-//     } else {
-//       res.end("There was an error");
-//     }
-//   });
-// });
 
 app.post("/", (req, res) => {
   location = req.body.search;
 
   const URI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}&units=imperial`;
 
-  console.log(URI)
-  
   request({ url: URI, json: true }, (err, response, body) => {
     if (!err) {
-
       if (body.cod == "404") {
-        //this is error handling if we DO get a response (unknown city for example), and theres not data we can use
         res.render("404");
       }
-      console.log("sending results")
+      console.log("sending results");
 
       res.render("weather", {
         city: req.body.search.toUpperCase(),
         weatherData: body,
       });
-      
     } else {
       res.end("There was an error");
     }
   });
 });
 
-//404 error if no other pages are found first
 app.get("/404", (req, res) => {
   res.render("404");
 });
 
-//start local server
 app.listen(port, () => {
   console.log("App is listening on port 3000");
 });
